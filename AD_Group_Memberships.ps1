e27cf5ed5ac6efca7263183f9f35da122b804473
Zerophen <60486960+Zerophen@users.noreply.github.com>
2020-05-28 09:46:14 -0500

    Update AD_Group_Memberships.ps1
    Clean up

--- AD_Group_Memberships.ps1
+++ AD_Group_Memberships.ps1
@@ -1,11 +1,10 @@
 $ADGroups = Get-ADGroup -Filter * 
-$i = 0
 $CSVData = @()
 $DateTime = Get-Date -f "yyyy-MM"
 $CSVFile = "C:\temp\AD_Groups"+$DateTime+".csv" 
 $member = $NULL
 
-foreach ($ADGroup in $ADGroups)
+ForEach ($ADGroup in $ADGroups)
 {
      $membersArr = @() 
      $MembersArr = Get-ADGroup -filter {Name -eq $ADGroup.Name} | Get-ADGroupMember | select Name
@@ -17,8 +16,6 @@ foreach ($ADGroup in $ADGroups)
         $newobj | add-member -MemberType NoteProperty -Name Scope -Value $ADGroup.GroupScope
         $newobj | add-member -MemberType NoteProperty -Name Members -Value $Member.Name
         $CSVData += $newobj
-        #$i++
-    } 
+     } 
 }
- $CSVData  | Sort-Object Name,Members | Export-Csv $CSVFile -NoTypeInformation 
-    #$newobj | add-member -MemberType NoteProperty -Name $t -Value  $currentUserProperties.$t
+ $CSVData  | Sort-Object Name,Members | Export-Csv $CSVFile -NoTypeInformation
